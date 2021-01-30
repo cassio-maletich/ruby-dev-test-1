7cf1e934cdb5f0fb0672fed4e0fb6de198c7b8a6
Cássio Maletich <cgm1010@gmail.com>
2021-01-30 12:17:51 -0300

    test setup to respect DRY

--- test/models/folder_test.rb
+++ test/models/folder_test.rb
@@ -1,9 +1,12 @@
 require 'test_helper'
 
 class FolderTest < ActiveSupport::TestCase
+  setup do 
+    @root_folder = Folder.first
+  end
+  
   test "create" do
-    root_folder = Folder.first
-    folder = Folder.new(name: "Folder 1", parent_id: root_folder.id)
+    folder = Folder.new(name: "Folder 1", parent_id: @root_folder.id)
     assert folder.valid?
   end
 
@@ -14,35 +17,30 @@ class FolderTest < ActiveSupport::TestCase
   end
 
   test "shouldn't create 2 - missing name" do
-    root_folder = Folder.first
-    folder = Folder.new(parent_id: root_folder.id)
+    folder = Folder.new(parent_id: @root_folder.id)
     assert_not folder.valid?
     assert_not_empty folder.errors[:name]
   end
 
   test "parent" do
-    root_folder = Folder.first
-    folder = Folder.new(name: "Folder 1", parent_id: root_folder.id)
-    assert_equal root_folder, folder.parent
+    folder = Folder.new(name: "Folder 1", parent_id: @root_folder.id)
+    assert_equal @root_folder, folder.parent
   end
 
   test "grandparent" do
-    root_folder = Folder.first
-    folder1 = Folder.create(name: "Folder 1", parent: root_folder)
+    folder1 = Folder.create(name: "Folder 1", parent: @root_folder)
     folder2 = Folder.create(name: "Folder 2", parent: folder1)
-    assert_equal root_folder, folder2.parent.parent
+    assert_equal @root_folder, folder2.parent.parent
   end
 
   test "child" do
-    root_folder = Folder.first
-    folder = Folder.create(name: "Folder 1", parent: root_folder)
-    assert root_folder.children.include? folder
+    folder = Folder.create(name: "Folder 1", parent: @root_folder)
+    assert @root_folder.children.include? folder
   end
 
   test "grandchild" do
-    root_folder = Folder.first
-    folder1 = Folder.create(name: "Folder 1", parent: root_folder)
+    folder1 = Folder.create(name: "Folder 1", parent: @root_folder)
     folder2 = Folder.create(name: "Folder 2", parent: folder1)
-    assert_equal folder2, root_folder.children.first.children.first
+    assert_equal folder2, @root_folder.children.first.children.first
   end
 end
