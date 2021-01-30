78820f8e3d9208c795e0d61d808797d27b9c4dc1
Cássio Maletich <cgm1010@gmail.com>
2021-01-30 14:38:46 -0300

    remove redundant validation

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,7 +1,6 @@
 class Folder < ApplicationRecord
     belongs_to :parent, class_name: "Folder"
     has_many :children, class_name: "Folder", foreign_key: "parent_id"
-    
+
     validates_presence_of :name
-    validates_presence_of :parent_id, message: "must have a parent"
 end
