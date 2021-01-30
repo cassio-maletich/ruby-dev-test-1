dccf11e03b2bd9f3417fae7d0e46ee72150a1e83
Cássio Maletich <cgm1010@gmail.com>
2021-01-30 11:55:47 -0300

    validate presence of fields

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,2 +1,4 @@
 class Folder < ApplicationRecord
+    validates_presence_of :name
+    validates_presence_of :parent_id, message: "must have a parent"
 end
--- test/models/folder_test.rb
+++ test/models/folder_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class FolderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create" do
+    root_folder = Folder.first
+    folder = Folder.new(name: "Folder 1", parent_id: root_folder.id)
+    assert folder.valid?
+  end
+
+  test "shouldn't create 1 - missing parent" do
+    folder = Folder.new(name: "Folder 1")
+    assert_not folder.valid?
+    assert_not_empty folder.errors[:parent_id]
+  end
+
+  test "shouldn't create 2 - missing name" do
+    root_folder = Folder.first
+    folder = Folder.new(parent_id: root_folder.id)
+    assert_not folder.valid?
+    assert_not_empty folder.errors[:name]
+  end
 end
